92c7da836a3e0ede3c431509b5ea1db68fa59212
Luciano Semicheche <lucianosemicheche@gmail.com>
2020-07-10 08:41:37 -0300

    Create mobile app with React-native and Expo

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import React from 'react';
+import { AppLoading } from 'expo'
 import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
+
+  if (!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+       <Routes />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#199',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+ size: {
+    fontSize: 20,
+    color: '#fff'
+ }
 });
